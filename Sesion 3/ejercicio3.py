79535ff5b602e0315f02229d75e2898219a8ce02
wetalff <alfredito.osejo@gmail.com>
2025-04-25 08:15:52 -0600

    Ejercicios semana 5 sesion 3

---
+++ Sesion 3/ejercicio3.py
@@ -0,0 +1,26 @@
+#semana
+numero = int(input("Ingrese un numero del 1 al 7: "))
+
+
+if numero == 1:
+    print("El dia de la semana es: Lunes")
+elif numero == 2:
+    print("El dia de la semana es: Martes")
+elif numero == 3:
+    print("El dia de la semana es: Miercoles")
+elif numero == 4:
+    print("El dia de la semana es: Jueves")
+elif numero == 5:
+    print("El dia de la semana es: Viernes")
+elif numero == 6:
+    print("El dia de la semana es: Sabado")
+elif numero == 7:
+    print("El dia de la semana es: Domingo")
+else:
+    print("Numero Invalido")
+    
+          
+    
+
+
+             
